8012523da7435f6bf1ccff5f33aef5e4d10ddfa7
pavelkulak <kulak-pavel@yandex.ru>
2023-07-07 19:42:59 +0300

    complete task8

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,31 +1,8 @@
-﻿// // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-// // 5 -> 2, 4
-// // 8 -> 2, 4, 6, 8
-// Console.Write("Введите число - ");
-// int N = int.Parse(Console.ReadLine());
-// int[] mass = new int[N];
-// // //if (N % 2 == 0)
-// // //{
-// for (int i = 1; i < N - 1; i += 2)
-// {
-//     mass[i] = i + 1;
+﻿Console.Write("Введите число N: ");
+        int N = int.Parse(Console.ReadLine());
 
-// }
-// for (int i = 1; i < N - 1; i += 2)
-// {
-//     Console.Write(mass[i]);
-// }
-
-Console.Write("Введите число N: ");
-int N = int.Parse(Console.ReadLine());
-
-int[] mass = new int[N];
-
-for (int i = 1; i < N - 1; i += 2)
-{
-    mass[i] = i + 1;
-}
-
-Console.WriteLine("Массив от 1 до N:");
-string joinedString = string.Join(", ", mass);
-Console.WriteLine(joinedString);
+        Console.WriteLine("Чётные числа от 1 до {0}:", N);
+        for (int i = 2; i <= N; i += 2)
+        {
+            Console.WriteLine(i);
+        }
